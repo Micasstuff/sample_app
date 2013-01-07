16a6c935be424b3feeffe8809e58aee7e3f33274
Micasstuff <micasstuff@gmail.com>
2013-01-07 01:05:26 +0000

    changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	#Full title return
+	def full_title(page_title)
+		base_title = 'RoR Tuts'
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,33 +4,37 @@ describe "Static pages" do
 
 	describe "Home page" do
 		it "should have the h1 'Home Page'" do
-		visit '/static_pages/home'
-		page.should have_selector('h1', :text => 'Home Page')
+			visit '/static_pages/home'
+			page.should have_selector('h1', :text => 'Home Page')
 		end
-		it "should have the right title" do
-		visit '/static_pages/home'
-		page.should have_selector('title', :text => "RoR Tuts | Home")
+		it "should have the base title" do
+			visit '/static_pages/home'
+			page.should have_selector('title', :text => "RoR Tuts")
+		end
+		it "should not have a custom page title" do
+			visit '/static_pages/home'
+			page.should_not have_selector('title', :text => '| Home')
 		end
 	end
 	
 	describe "Help page" do
 		it "should have the h1 'Help'" do
-		visit '/static_pages/help'
-		page.should have_selector('h1', :text => 'Help')
+			visit '/static_pages/help'
+			page.should have_selector('h1', :text => 'Help')
 		end
 	end
 
 	describe "About Us" do
 		it "should have the h1 'About Us'" do
-		visit '/static_pages/about'
-		page.should have_selector('h1', :text => 'About Us')
+			visit '/static_pages/about'
+			page.should have_selector('h1', :text => 'About Us')
 		end
 	end
 	
 	describe "Contact Us" do
 		it "should have the h1 'Contact Us'" do
-		visit '/static_pages/contact'
-		page.should have_selector('h1', :text => 'Contact Us')
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text => 'Contact Us')
 		end
 	end
 
