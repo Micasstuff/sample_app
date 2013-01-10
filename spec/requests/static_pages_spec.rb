5985de7d876262f17d207edbaa721407bef0dd2b
Micasstuff <micasstuff@gmail.com>
2013-01-10 12:27:23 +0000

    tests working

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -27,21 +27,24 @@ describe "Static pages" do
 	
 	describe "Contact Us" do
 		before { visit contact_path }
-		it { should have_selector('h1', :text => 'Contact Us')}
-		it { should have_selector('title', text:  full_title('Contact Us')) }
+		it { should have_selector('h1', :text => 'Contact')}
+		it { should have_selector('title', text:  full_title('Contact')) }
 	end
 
     it "should have the right links on the layout" do
 	    visit root_path
+	    
 	    click_link "About"
 	    page.should have_selector 'title', text: full_title('About Us')
 	    click_link "Help"
 	    page.should have_selector 'title', text: full_title('Help')
 	    click_link "Contact Us"
 	    page.should have_selector 'title', text: full_title('Contact')
-	    click_link "SignIn  "
-	    click_link "Sign up now!"
-	    page.should have_selector 'title', text: full_title('Sign up')
+	    click_link "SignIn"
+	    page.should have_selector 'title', text: full_title('Sign Up')
+	    click_link "Home"
+	    click_link "Sign Up!"
+	    page.should have_selector 'title', text: full_title('Sign Up')
 	    click_link "Home"
 	    page.should have_selector 'h1', text: 'Home'
 	end
