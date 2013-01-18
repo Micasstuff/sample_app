a72afa0be88006992443ad029ab3bd640bf1d20d
Micasstuff <micasstuff@gmail.com>
2013-01-18 20:35:36 +0000

    signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,4 +11,12 @@ describe "UserPages" do
 		it { should have_selector('title', text:  full_title('Sign Up')) }
 	
 	end 
+	
+	describe "profile page" do
+		before { visit user_path() }
+
+		it { should have_selector('h1',    text: user.name) } 
+		it { should have_selector('title', text: user.name) }
+	
+	end 
 end
